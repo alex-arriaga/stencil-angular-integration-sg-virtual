7882b274f7661607c57d1f8e48a7c050c7ee86b9
Alex Arriaga <alex.arriaga@webtraining.world>
2020-12-03 20:43:28 -0800

    Add StencilJS integration

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Note: loader import location set using "esmLoaderPath" within the output target config
+import { defineCustomElements } from '@alex-arriaga/stencil-sg-virtual-2020/loader';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+defineCustomElements();
